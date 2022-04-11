ea24baceb3c2f689ba6be2a728efad399c5f085a
Maro de Melo <maromelo@tempus.digital>
2022-04-11 18:16:22 -0300

    criado componente comentario

--- App.js
+++ App.js
@@ -6,10 +6,26 @@ import Post from './src/components/Post'
   
 export default class App extends Component {
   render(){
+    const comments = [{
+      nickname: 'Maro de Melo',
+      comment: 'Excelente foto'
+    },
+    { 
+      nickname: 'Joyci Santos',
+      comment: 'Irado'
+    },
+    { 
+      nickname: 'Gustavo Ricardo',
+      comment: 'Muito feio. Cerca sem noção'
+    }
+  ]
+
+
     return (
       <View style={{flex:1}}>
       <Header/>
-      <Post image={require('./assets/imgs/fence.jpg')}/>
+      <Post image={require('./assets/imgs/fence.jpg')}
+      comments={comments}/>
       </View>
     );
   }
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,6 +1,7 @@
 import React, { Component} from 'react'
 import {StyleSheet, View, Image, Dimensions} from 'react-native'
 import Author from './Author'
+import Comments from './Comments'
 
 class Post extends Component {
     render (){
@@ -8,6 +9,7 @@ class Post extends Component {
             <View style={styles.container}>
                 <Image source={this.props.image} style={styles.image}/>
                 <Author email='maro_fis@hotmail.com' nickname='Maro de Melo'></Author>
+                <Comments comments={this.props.comments}/>
             </View>
         )
     }
