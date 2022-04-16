e390c41fa0578ec02a449cfef3daeb74771eebf5
immerfallen <marodemelo10@gmail.com>
2022-04-16 16:19:22 -0300

    criado componente de adicionar foto

--- src/Navigator.js
+++ src/Navigator.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
 import Icon from 'react-native-vector-icons/FontAwesome'
-import Feed from './screens/Feed'
 import { NavigationContainer } from '@react-navigation/native';
+
+import Feed from './screens/Feed'
+import AddPhoto from './screens/AddPhoto';
 
 const Tabs = createBottomTabNavigator();
 
@@ -20,9 +22,8 @@ function MyTabs(){
                     iconName = 'camera';
                   } else if (route.name ==='Profile'){
                       iconName = 'user'
-                  }
-      
-                  // You can return any component that you like here!
+                  }      
+                  
                   return <Icon name={iconName} size={30} color={color} />;
                 },
                 tabBarActiveTintColor: 'blue',
@@ -32,52 +33,12 @@ function MyTabs(){
               })}
             >
                 <Tabs.Screen name='Feed' component={Feed}/>
-                <Tabs.Screen name='Camera' component={Feed}/>
+                <Tabs.Screen name='Camera' component={AddPhoto}/>
                 <Tabs.Screen name='Profile' component={Feed}/>
             </Tabs.Navigator>
         </NavigationContainer>
     )
 }
-
-// const MenuRoutes = {
-//     Feed: {
-//         name: 'Feed',
-//         screen: Feed,
-//         navigationOptions: {
-//             title: 'Feed',
-//             tabBarIcon: ({tintColor  }) =>
-//             <Icon name='home' size={30} color={tintColor}/>
-//         }
-//     },
-//     Add: {
-//         name: 'AddPhoto',
-//         screen: Feed,
-//         navigationOptions: {
-//             title: 'Add Picture',
-//             tabBarIcon: ({tintColor  }) =>
-//             <Icon name='camera' size={30} color={tintColor}/>
-//         }
-//     },
-//     Profile: {
-//         name: 'Profile',
-//         screen: Feed,
-//         navigationOptions: {
-//             title: 'Profile',
-//             tabBarIcon: ({tintColor  }) =>
-//             <Icon name='user' size={30} color={tintColor}/>
-//         }
-//     }
-// }
-
-// const MenuConfig = {
-//     initialRouteName: 'Feed',
-//     tabBarOptions: {
-//         showLabel: false,
-
-//     }
-// }
-
-// const MenuNavigator = createAppContainer(createBottomTabNavigator(MenuRoutes, MenuConfig))
 
 export default MyTabs
 
---
+++ src/screens/AddPhoto.js
@@ -0,0 +1,108 @@
+import React, {Component} from 'react'
+import {View, Text, PermissionsAndroid, StyleSheet, TouchableOpacity, Image, Dimensions, Platform, ScrollView, Alert, TextInput} from 'react-native'
+
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker'
+
+class AddPhoto extends Component {
+    state ={
+        image: {
+            uri: null,
+            base64: null,
+        },
+        comment: '',
+    }    
+      
+    pickImage = () => {
+        launchImageLibrary({
+            maxHeight: 600,
+            maxWidth: 800,
+        }, res => {
+            if(!res.didCancel){
+                console.log(res)
+                this.setState({image: {uri: res.assets[0].uri, base64: res.assets[0].base64}})
+            }
+        }, 
+        )           
+    }
+
+    takePhoto = async () => {       
+            launchCamera({
+                saveToPhotos: true, 
+                maxHeight: 600,
+                maxWidth: 800           
+            }, res => {
+                if(!res.didCancel){
+                    console.log(res.assets.uri)
+                    this.setState({image: {uri: res.assets[0].uri, base64: res.assets[0].base64}})
+                   
+                }
+            }) 
+        
+           
+    }
+
+    save = async () => {
+        Alert.alert('Imagem foi adicionada!', this.state.comment)
+    }
+
+    render(){
+        return(
+            <ScrollView>
+                <View style={styles.container}>
+                    <Text style={styles.title}>Compartilhe uma imagem!!</Text>
+                    <View style={styles.imageContainer}>
+                        <Image source={this.state.image} style={styles.image}/>
+                    </View>
+                    <TouchableOpacity onPress={this.pickImage} style={styles.button}>
+                        <Text style={styles.buttonText}>Escolha uma foto da galeria</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={this.takePhoto} style={styles.button}>
+                        <Text style={styles.buttonText}>Tirar uma foto</Text>
+                    </TouchableOpacity>
+                    <TextInput placeholder='Adicione um comentário' style={styles.input} value={this.state.comment} onChangeText={comment=> this.setState({comment})}></TextInput>
+                    <TouchableOpacity onPress={this.save} style={styles.button}>
+                        <Text style={styles.buttonText}>Salvar</Text>
+                    </TouchableOpacity>
+                </View>
+            </ScrollView>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center'
+    },
+    title: {
+        fontSize: 20,
+        marginTop: Platform.OS === 'ios' ? 30 : 10,
+        fontWeight: 'bold'
+    },
+    imageContainer: {
+        width: '90%',
+        height: Dimensions.get('window').width*3/4,
+        backgroundColor: '#EEE',
+        marginTop: 10,
+    },
+    image: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').width*3/4,
+        resizeMode: 'center'
+    }, 
+    button: {
+        marginTop: 30,
+        padding: 10,
+        backgroundColor: '#4286f4'
+    },
+    buttonText: {
+        fontSize: 20,
+        color: '#FFF'
+    },
+    input:{
+        marginTop: 20,
+        width: '90%'
+    }
+})
+
+export default AddPhoto
